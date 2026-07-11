1041fa7592120b82492488235d06b39f03be2a48
kashifniaxi <kashifkhan0516@gmail.com>
2026-07-11 23:36:23 +0500

    chore(resolvers/index): merge all resolvers (auth, user, tour, booking, review) into a single root Query and Mutation map

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,18 +1,19 @@
 import authResolver from "./auth.resolver.js";
-import reviewResolver from "./review.resolver.js";
+import userResolver from "./user.resolver.js";
 import tourResolver from "./tour.resolver.js";
 import bookingResolver from "./booking.resolver.js";
-
-import userResolver from "./user.resolver.js";
+import reviewResolver from "./review.resolver.js";
 
 export default {
-    Query: {
-        ...userResolver.Query,
-    },
-    Mutation : {
-        ...authResolver.Mutation,
-        ...reviewResolver.Mutation,
-        ...tourResolver.Mutation,
-        ...bookingResolver.Mutation,
-    },
+  Query: {
+    ...userResolver.Query,
+    ...tourResolver.Query,
+  },
+  Mutation: {
+    ...authResolver.Mutation,
+    ...userResolver.Mutation,
+    ...tourResolver.Mutation,
+    ...bookingResolver.Mutation,
+    ...reviewResolver.Mutation,
+  },
 };
