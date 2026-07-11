6203daffaf236e0d8a2fa2b7ce86781013529486
kashifniaxi <kashifkhan0516@gmail.com>
2026-07-11 23:35:43 +0500

    feat(booking.schema): define bookTour, leaveTour mutations and bookings query with Booking type linked to User and Tour

--- server/src/graphql/schema/booking.schema.js
+++ server/src/graphql/schema/booking.schema.js
@@ -1,23 +1,23 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-    type Booking {
-        id: ID!
-        tour: Tour!
-        user: User!
-        status: String!
-    }
+  type Booking {
+    id: ID!
+    tour: Tour!
+    user: User!
+    status: String!
+  }
 
-    input BookTourInput {
-        tourId: ID!
-    }
+  input BookTourInput {
+    tourId: ID!
+  }
 
-    type Query {
-        bookings: [Booking!]!
-    }
+  extend type Query {
+    bookings: [Booking!]!
+  }
 
-    type Mutation {
-        bookTour(input: BookTourInput!): Booking!
-        leaveTour(bookingId: ID!): Boolean!
-    }
+  extend type Mutation {
+    bookTour(input: BookTourInput!): Booking!
+    leaveTour(bookingId: ID!): Boolean!
+  }
 `;
